9693baa4e01e10c48fe71d10600ac98ec6f68238
Nick Little <nicklaus.little@us.af.mil>
2021-08-02 14:30:04 -0500

    Add older Ubuntu C++ images

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Project 1 Build Images
-The project 1 build images are created using the dockerfiles in this repository.
+# Build Images
+The 76 EDDGE build images are created using the dockerfiles in this repository. They are pieced together into Kubernetes Pods and used to build separate components of different projects.
 
 ## Building
 All building is handled by the build.sh script. The images are only pushed to dockerhub when a release is created.
--- build.sh
+++ build.sh
@@ -17,7 +17,9 @@ if [ $# -gt 0 ]; then
   exit 1
 fi
 
-# docker buildx build . --file Dockerfile --tag horseshoe/horseshoe --tag horseshoe/horseshoe:${GITHUB_REF/refs\/tags\//} --platform linux/amd64,linux/arm/v7,linux/arm64 --build-arg DEPLOY=local-jar --build-arg JAR_FILE=build/libs/horseshoe-*.jar --target deploy --push
-# docker build cxx --tag 76eddge/cxx --target deploy
-docker build cxx-CentOS6 --tag 76eddge/cxx:CentOS6 --target deploy
-if [ ! -z "$PUSH" ]; then docker push -a 76eddge/cxx; fi
+docker build cxx --tag 76eddge/build-images:cxx-Xenial --target deploy
+docker build cxx-Trusty --tag 76eddge/build-images:cxx-Trusty --target deploy
+# CentOS 6 is EOL, so this image will not change and doesn't need to be rebuilt
+docker build cxx-CentOS6 --tag 76eddge/build-images:cxx-CentOS6 --target deploy
+
+if [ ! -z "$PUSH" ]; then docker push -a 76eddge/build-images; fi
