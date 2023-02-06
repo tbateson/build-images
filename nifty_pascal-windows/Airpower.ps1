a56a6ba454cf23452730dd556a3b84ae98aea785
Nick Little <nicklaus.little@us.af.mil>
2023-02-06 09:11:20 -0600

    New Airpower for nifty_pascal-windows

---
+++ nifty_pascal-windows/Airpower.ps1
@@ -0,0 +1,16 @@
+$script:AirpowerPackages =
+	'cmake-converter',
+	'cmake',
+	'doxygen',
+	'erlang',
+	'git',
+	'gradle',
+	'jdk',
+	'jom',
+	'llvm',
+	'nasm',
+	'ninja',
+	'node',
+	'perl',
+	'python',
+	"vs-buildtools"
